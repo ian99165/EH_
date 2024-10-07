a0a1e2d5f42ae18a35cf6d75aa185e7afd009cfe
opportunit26 <opportunit26@gmail.com>
2024-10-07 16:49:09 +0800

    renew

--- Assets/Script/listTest.cs
+++ Assets/Script/listTest.cs
@@ -8,8 +8,8 @@ public class listTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        txtList.Add("洋娃娃");
-        txtList.Add("呀！");
+        txtList.Add("洋娃娃:");
+        txtList.Add("你好呀！");
         foreach (var line in txtList)
         {
             Debug.Log(line);
