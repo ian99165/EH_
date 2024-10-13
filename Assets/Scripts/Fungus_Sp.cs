dc890af462cca363400fd7291b154ebd4f156c54
ian99165 <opportunit26@gmail.com>
2024-10-14 02:00:35 +0800

    Add Fungus_Sp.cs and Flowchart logic 10/14 //目前需要射線控制(急)

---
+++ Assets/Scripts/Fungus_Sp.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+
+public class Fungus_Sp : MonoBehaviour
+{
+    public string PlayerName = "Player";
+    private bool hasTalk = false;
+    
+    private bool isDollTalk;
+    private bool isLaydTalk_I;
+    private bool isLaydTalk_II;
+    
+    public Flowchart flowchart;
+
+    private void Start()
+    {
+        isDollTalk = true;
+    }
+
+    private void Update()
+    {
+        Talking();
+    }
+
+    private void Talking()
+    {
+        if(!hasTalk)
+        {
+            hasTalk = true;
+            if (isDollTalk)
+            {
+                flowchart.ExecuteBlock("Doll_I_I");
+            }
+            
+            if (isLaydTalk_I)
+            {
+                flowchart.ExecuteBlock("Layd_I_I");
+            }
+            
+            if (isLaydTalk_II)
+            {
+                flowchart.ExecuteBlock("Layd_I_I");
+            }
+        }
+    }
+}
