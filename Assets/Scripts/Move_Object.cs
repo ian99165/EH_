5df1aa94c3e71a01986c7414ebf3e2e8ae66b6cd
ian99165 <opportunit26@gmail.com>
2024-10-21 14:58:01 +0800

    待製 物件移動

---
+++ Assets/Scripts/Move_Object.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Object : MonoBehaviour
+{
+
+    public void SI_Door_I()
+    {
+        //door 右移
+        Debug.Log("SI_Door_I");
+    }
+
+    public void SI_Object_I_I()
+    {
+        //抽屜推出
+        Debug.Log("SI_Object_I_I");
+
+    }
+}
