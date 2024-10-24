39898d90e63e9727d7cd6eb7363d57828756c028
ian99165 <opportunit26@gmail.com>
2024-10-24 11:26:20 +0800

    fungus 素材包刪除

--- Assets/Plugins/Fungus/Scripts/Editor/ViewEditor.cs
+++ Assets/Plugins/Fungus/Scripts/Editor/ViewEditor.cs
@@ -137,7 +137,7 @@ namespace Fungus.EditorUtils
 
             for (int i = 0; i < 2; ++i)
             {
-                var fmh_141_57_638639444811370631 = Quaternion.identity; Vector3 newPos = Handles.FreeMoveHandle(handles[i],
+                var fmh_141_57_638653658114530807 = Quaternion.identity; Vector3 newPos = Handles.FreeMoveHandle(handles[i],
                                                         HandleUtility.GetHandleSize(pos) * 0.1f,
                                                         Vector3.zero,
 #if UNITY_5_6_OR_NEWER
