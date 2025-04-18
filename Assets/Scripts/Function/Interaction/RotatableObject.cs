bf39ea923a67bf6066ec45119b2cf10f62ad43d5
ian99165 <opportunit26@gmail.com>
2025-04-19 02:37:25 +0800

    1

--- Assets/Scripts/Function/Interaction/RotatableObject.cs
+++ Assets/Scripts/Function/Interaction/RotatableObject.cs
@@ -1,72 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RotatableObject : MonoBehaviour
 {
     public float rotationSpeed = 300f;
     public float mouseSpeed = 100f;
+    public InputActionReference moveAction;
     private Vector3 lastMousePosition;
 
     void Update()
     {
-        RotateWithMouse();
-        RotateWithArrowKeys(); // 方向鍵旋轉
-    }
-
-    public void RotateWithMouse() // 滑鼠旋轉物件
-    {
         if (gameObject.CompareTag("Item"))
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                lastMousePosition = Input.mousePosition;
-            }
-
-            if (Input.GetMouseButton(0))
-            {
-                Vector3 delta = Input.mousePosition - lastMousePosition;
-                float rotationX = delta.x * mouseSpeed * Time.deltaTime;
-                float rotationY = delta.y * mouseSpeed * Time.deltaTime;
-
-                transform.Rotate(Vector3.up, -rotationX, Space.World);
-                transform.Rotate(Vector3.right, rotationY, Space.World);
-
-                lastMousePosition = Input.mousePosition;
-            }
+            RotateWithMouse();
+            RotateWithStick();
         }
     }
 
-    public void RotateWithArrowKeys() // 方向鍵旋轉物件
+    public void RotateWithMouse()
     {
-        if (gameObject.CompareTag("Item"))
+        if (Input.GetMouseButtonDown(0))
         {
-            float rotationX = 0f;
-            float rotationY = 0f;
+            lastMousePosition = Input.mousePosition;
+        }
 
-            // 左右鍵旋轉 Y 軸
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                rotationX = -rotationSpeed * Time.deltaTime;
-            }
-            else if (Input.GetKey(KeyCode.RightArrow))
-            {
-                rotationX = rotationSpeed * Time.deltaTime;
-            }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 delta = Input.mousePosition - lastMousePosition;
+            float rotationX = delta.x * mouseSpeed * Time.deltaTime;
+            float rotationY = delta.y * mouseSpeed * Time.deltaTime;
 
-            // 上下鍵旋轉 X 軸
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                rotationY = rotationSpeed * Time.deltaTime;
-            }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                rotationY = -rotationSpeed * Time.deltaTime;
-            }
-
-            // 套用旋轉
-            transform.Rotate(Vector3.up, rotationX, Space.World);
+            transform.Rotate(Vector3.up, -rotationX, Space.World);
             transform.Rotate(Vector3.right, rotationY, Space.World);
+
+            lastMousePosition = Input.mousePosition;
         }
+    }
+
+    public void RotateWithStick()
+    {
+        Vector2 input = moveAction.action.ReadValue<Vector2>();
+
+        float rotationX = input.x * rotationSpeed * Time.deltaTime;
+        float rotationY = input.y * rotationSpeed * Time.deltaTime;
+
+        transform.Rotate(Vector3.up, rotationX, Space.World);
+        transform.Rotate(Vector3.right, -rotationY, Space.World);
     }
 }
